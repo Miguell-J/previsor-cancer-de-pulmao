569d6565cb5a72f424a0944c3245bda7178f8d72
Miguel Julio <138534658+Miguell-J@users.noreply.github.com>
2023-11-14 16:14:45 -0300

    Update app_lung_cancer.py

--- app_lung_cancer.py
+++ app_lung_cancer.py
@@ -4,9 +4,10 @@ import pandas as pd
 import numpy as np
 from streamlit.components.v1 import html
 import matplotlib.pyplot as plt
+import os
 
-
-model = jb.load('LogisticRegressor.joblib')
+model_path = os.path.join(os.path.dirname(__file__), 'LogisticRegressor.joblib')
+model = jb.load(model_path)
 
 
 image_url = "https://www.bhtorax.com.br/wp-content/uploads/2021/01/O-que-e-o-cancer-de-pulmao_-Foto-5.jpg"
